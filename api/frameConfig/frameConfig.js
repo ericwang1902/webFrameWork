63c9826ab86a621d9eab365eef76ad1525ab19cd
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-02-17 22:49:09 +0800

    修改

--- api/frameConfig/frameConfig.js
+++ api/frameConfig/frameConfig.js
@@ -15,7 +15,7 @@ module.exports={
     wechatMenuURL:"https://api.weixin.qq.com/cgi-bin/menu/addconditional?access_token=",
     //微信用户标签url
     wechatTagURL:"https://api.weixin.qq.com/cgi-bin/tags/create?access_token=",
-    //获取微信全局的accesstoken的网址
-    wechatTokenURL:"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+this.wechatConfig.appid+"&secret="+this.wechatConfig.appsecret
+    //获取微信全局的accesstoken的网址，这需要修改！！！！！
+    wechatTokenURL:"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx94e3a09e4149b262&secret=6e428e808f1620210ef32a2d2313a038"
 
 }
