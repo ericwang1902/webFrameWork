ac826923a56a73e1bec3f1b4438e6b80b92da9aa
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-02-18 23:59:59 +0800

    定时任务测试

--- api/common/seed.js
+++ api/common/seed.js
@@ -194,10 +194,14 @@ var initData = function () {
 var setSchedule=function(){
     console.log("开始定时任务")
     wechatApi.getApiToken();
-    schedule.scheduleJob('* 50 * * * *', function(){
+
+    var rule2     = new schedule.RecurrenceRule();  
+    var times2    = [1,3,5,7,9,11,13,15,17,19,21,23];  
+    rule2.minute  = times2;  
+    schedule.scheduleJob(rule2, function(){  
         console.log('定时任务:' + new Date());
-        wechatApi.getApiToken();
-    }); 
+        wechatApi.getApiToken(); 
+    });  
 }
 
 
