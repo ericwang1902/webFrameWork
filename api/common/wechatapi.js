9ca859a565c3059cf04d1fa81f169045952ff04b
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-02-19 11:05:22 +0800

    测试

--- api/common/wechatapi.js
+++ api/common/wechatapi.js
@@ -34,32 +34,11 @@ function createMenu() {
                     type: "click", 
                     name: "今日歌曲", 
                     key: "V1001_TODAY_MUSIC"
-                }, 
-                {
-                    name: "菜单", 
-                    sub_button: [
-                        {
-                            type: "view", 
-                            name: "搜索", 
-                            url: "http://www.soso.com/"
-                        }, 
-                        {
-                            type: "view", 
-                            name: "视频", 
-                            url: "http://v.qq.com/"
-                        }, 
-                        {
-                            type: "click", 
-                            name: "赞一下我们", 
-                            key: "V1001_GOOD"
-                        }
-                    ]
                 }
             ], 
             matchrule: {
                 tag_id: "101"
-            }
-        
+            }      
         }
     }
     request(menuOptions, function (err, response, body) {
