3c2e75480d967f36c26120edd10e5680aabb550b
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-02-20 21:44:12 +0800

    debug

--- api/common/dbutils.js
+++ api/common/dbutils.js
@@ -5,12 +5,13 @@ var mongoose = require('mongoose')
 
 var Config = require('../frameConfig/frameConfig')
 
+mongoose.Promise = require('bluebird');
+
 module.exports = {
     createconnection: function () {
         var options = {
             user: 'ericwang1903',
-            pass: 'qwer12345',
-            promiseLibrary: require('bluebird')
+            pass: 'qwer12345'
         }
         mongoose.connect('mongodb://localhost:20008/' + Config.databaseName,options);
     },
