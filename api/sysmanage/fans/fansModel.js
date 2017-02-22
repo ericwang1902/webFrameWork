fb0171c893cbeaab56f202d39fe1b594946d7400
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-02-22 22:45:43 +0800

    修改

--- api/sysmanage/fans/fansModel.js
+++ api/sysmanage/fans/fansModel.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var fansSchema = new Schema({	'fannickname' : String,	'fanopenid' : String,	'orders' : Array,	'points' : Number,	'coupons' : Array});
+var fansSchema = new Schema({
+	'fannickname' : String,
+	'fanopenid' : String,
+	'orders' : Array,
+	'points' : Number,
+	'coupons' : Array
+});
 
 module.exports = mongoose.model('fans', fansSchema);
