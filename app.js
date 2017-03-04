6bd52a887814e10b1ac9295499f87408e44b9ddf
ericwang1902 <2248930513@qq.com>
2017-03-04 11:03:17 +0800

    增加微信初始化

--- app.js
+++ app.js
@@ -137,8 +137,8 @@ var seed = require('./api/common/seed');
 seed.initData();
 
 //下面两个是微信相关的，在本地调试的时候，先注释掉
-// seed.setSchedule();//设置定时获取apitoken的定时任务
-// seed.initTagAndMenu();//初始化tag、菜单
+seed.setSchedule();//设置定时获取apitoken的定时任务
+seed.initTagAndMenu();//初始化tag、菜单
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
