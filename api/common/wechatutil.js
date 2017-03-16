0af867b957cd3cec6e78f95b41a99716d8a9817e
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-03-16 23:18:42 +0800

    修改微信端代码

---
+++ api/common/wechatutil.js
@@ -0,0 +1,82 @@
+//非全局微信对接的方法
+
+var fansModel = require('../sysmanage/fans/fansModel');
+
+
+var OAuth = require('wechat-oauth');
+var config = require('../frameConfig/frameConfig');
+var client = new OAuth(config.wechatConfig.appid, config.wechatConfig.appsecret);
+
+//第三方库获取openid和user_access_token
+var getopenid = function (req, res, next) {
+    //如果session中的user_access_token已经过期
+    console.log("session中的user_access_token:" + req.session.user_access_token);
+    if (!req.session.user_access_token) {
+
+        client.getAccessToken(req.query.code, function (err, result) {
+            try {
+                var openid = result.data.openid;
+                var accessToken = result.data.access_token;
+
+                req.session.openid = openid;
+                req.session.user_access_token = accessToken;
+                console.log("获取openid：" + req.session.openid)
+                console.log("获取access token：" + req.session.user_access_token)
+
+
+            } catch (error) {
+                console.log(err)
+                return res.json({
+                    info: '获取openid错误'
+                })
+            }
+            return next();
+
+        });
+    } else {
+        console.log("已经创建了该用户!")
+        return next();
+    }
+}
+
+
+//创建粉丝档案
+var createFans = function (req, res, next) {
+    fansModel.findOne({ fanopenid: req.session.openid }, function (err, fanresult) {
+        if (err) {
+            console.log("createFans err")
+            console.log(err);
+        }
+        //没有创建该fans就进行创建
+        if (!fanresult) {
+            var fans = new fansModel({
+                fannickname: '',
+                fanopenid: req.session.openid,
+                orders: [],
+                points: 0,
+                coupons: []
+            });
+            fans.save(function (err, fanSaveResult) {
+                if (err) {
+                    console.log(err);
+                }
+                //添加成功
+                req.fanSaveResult = fanSaveResult;
+                return next();
+            })
+        } else {
+            //如果该粉丝数据已经创建，就将fanSaveResult写进req
+            req.fanSaveResult = fanresult;
+            console.log("fanSaveResult:" + JSON.stringify(req.fanSaveResult))
+
+            return next();
+        }
+
+
+    })
+}
+
+module.exports = {
+    getopenid,
+    createFans
+}
--- api/mobile/index.js
+++ api/mobile/index.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var request = require('request');
-
-var config = require('../frameConfig/frameConfig')
-
 var fansModel = require('../sysmanage/fans/fansModel');
 
-//微信网页授权
-var OAuth = require('wechat-oauth');
-var config = require('../frameConfig/frameConfig');
-var client = new OAuth(config.wechatConfig.appid, config.wechatConfig.appsecret);
+var wechatutil = require('../common/wechatutil');
 
 var wechatapi = require('../common/wechatapi')
 
 var mobileRouter =require('./mobilerouter');
+
+
 
 // 这里的路由都是/mobile/xxx，不需要加sysmanage
 /*  index用来获取openid，并用来跳转到home */
@@ -25,7 +20,7 @@ router.get('/index', function (req, res, next) {
 
 
 // 通过跳转到home携带code，获取openid，只能用这种跳转的方式，不能用ajax访问获取openid
-router.get('/home', getopenid, createFans, function (req, res, next) {
+router.get('/home', wechatutil.getopenid, wechatutil.createFans, function (req, res, next) {
 
     //获取了fan的数据之后，判断该fan是否有district数据，如果没有，就跳转到initfans；如果有，就直接跳转到前端home路由
     if(!req.fanSaveResult.district){
@@ -47,6 +42,7 @@ router.get('/home', getopenid, createFans, function (req, res, next) {
    // res.redirect("http://localhost:8090/")
 });
 
+
 //mobile/bind 用来获取用户openid⭐️️️️️️️️️️
 //店主、配送员、区域代理的openid的绑定
 router.get('/bind', function (req, res, next) {
@@ -64,75 +60,6 @@ router.get('/bindjump',getopenid,function(req,res,next){
 router.use('/userbind',mobileRouter);//用户绑定
 
 
-//第三方库获取openid和user_access_token
-function getopenid(req, res, next) {
-    //如果session中的user_access_token已经过期
-    console.log("session中的user_access_token:" + req.session.user_access_token);
-    if (!req.session.user_access_token) {
-
-        client.getAccessToken(req.query.code, function (err, result) {
-            try {
-                var openid = result.data.openid;
-                var accessToken = result.data.access_token;
-
-                req.session.openid = openid;
-                req.session.user_access_token = accessToken;
-                console.log("获取openid：" + req.session.openid)
-                console.log("获取access token：" + req.session.user_access_token)
-
-
-            } catch (error) {
-                console.log(err)
-                return res.json({
-                    info: '获取openid错误'
-                })
-            }
-            return next();
-
-        });
-    } else {
-        console.log("已经创建了该用户!")
-        return next();
-    }
-
-
-}
-
-//创建粉丝档案
-function createFans(req, res, next) {
-    fansModel.findOne({ fanopenid: req.session.openid }, function (err, fanresult) {
-        if (err) {
-            console.log("createFans err")
-            console.log(err);
-        }
-        //没有创建该fans就进行创建
-        if (!fanresult) {
-            var fans = new fansModel({
-                fannickname: '',
-                fanopenid: req.session.openid,
-                orders: [],
-                points: 0,
-                coupons: []
-            });
-            fans.save(function (err, fanSaveResult) {
-                if (err) {
-                    console.log(err);
-                }
-                //添加成功
-                req.fanSaveResult = fanSaveResult;
-                return next();
-            })
-        } else {
-            //如果该粉丝数据已经创建，就将fanSaveResult写进req
-            req.fanSaveResult = fanresult;
-            console.log("fanSaveResult:" + JSON.stringify(req.fanSaveResult))
-
-            return next();
-        }
-
-
-    })
-}
 
 
 module.exports = router;
