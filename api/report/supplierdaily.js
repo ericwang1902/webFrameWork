7c490b2ae318f1e7b86371490462b028b9702b19
王海军 <EricWang@wanghaijundeMacBook-Air.local>
2017-04-12 00:11:05 +0800

    加入populate

--- api/report/supplierdaily.js
+++ api/report/supplierdaily.js
@@ -5,8 +5,17 @@ var async = require('async')
 
 var getSupplier = function (req, res) {
     shoporderModel.aggregate([
-        { $group: { _id: "$supplier", totalPop: { $sum: "$orderamount" } } }
-        ])
+        { $group: { _id: "$supplier", totalPop: { $sum: "$orderamount" } } },
+        {
+            $lookup:
+            {
+                from: 'supplierModel',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'supplier'
+            }
+        }
+    ])
         .exec(function (err, shoporders) {
             if (err) {
                 return res.status(500).json({
