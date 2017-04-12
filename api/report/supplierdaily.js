82668859ec1d407478d71f79677c9f857059d673
ericwang1902 <2248930513@qq.com>
2017-04-12 17:39:59 +0800

    debug

--- api/report/supplierdaily.js
+++ api/report/supplierdaily.js
@@ -5,23 +5,23 @@ var moment = require('moment');
 var async = require('async')
 
 var getSupplier = function (req, res) {
-        var today= moment().set({'hour': 0, 'minute': 0,'second':30});
-        console.log(today)
+    var today = moment().set({ 'hour': 0, 'minute': 0, 'second': 30 });
+    console.log(today)
 
-        shoporderModel.aggregate([
-            {$match:{$gte:today}},
-            { $group: { _id: { supplierid: "$supplier" }, orderamount: { $sum: "$orderamount" } } },
+    shoporderModel.aggregate([
+        { $match: { ordertime: { $gte: today } } },
+        { $group: { _id: { supplierid: "$supplier" }, orderamount: { $sum: "$orderamount" } } },
+        {
+            $lookup:
             {
-                $lookup:
-                {
-                    from: 'suppliers',//这个要不是model，而是suppliers，是collection的名字！！！
-                    localField: '_id.supplierid',
-                    foreignField: '_id',
-                    as: 'supplierdoc'
-                }
+                from: 'suppliers',//这个要不是model，而是suppliers，是collection的名字！！！
+                localField: '_id.supplierid',
+                foreignField: '_id',
+                as: 'supplierdoc'
             }
+        }
 
-        ])
+    ])
         .exec(function (err, shoporders) {
             if (err) {
                 return res.status(500).json({
