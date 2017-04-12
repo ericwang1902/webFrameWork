af5151fabddcedaab833b7b73b6775be391beca8
ericwang1902 <2248930513@qq.com>
2017-04-12 18:02:15 +0800

    debug

--- api/report/supplierdaily.js
+++ api/report/supplierdaily.js
@@ -20,7 +20,7 @@ var getSupplier = function (req, res) {
                 as: 'supplierdoc'
             }
         },
-        { $unwind: $supplierdoc }
+        { $unwind: "$supplierdoc" }
 
     ])
         .exec(function (err, shoporders) {
